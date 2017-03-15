7d19881db85b684912c73709b97291fca410bdaa
kursadbilgin <ceng.kursad.bilgin@gmail.com>
2017-03-15 15:25:23 +0300

    Bazı hatalar düzeltildi

--- edys/apps/journal/admin.py
+++ edys/apps/journal/admin.py
@@ -1,6 +1,8 @@
 # Django
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.admin.templatetags.admin_modify import *
+from django.contrib.admin.templatetags.admin_modify import submit_row as original_submit_row
 
 # Local Django
 from user.models import User
@@ -12,6 +14,15 @@ from core.variables import (
     USER_ASSIGNEDEDITOR, USER_REVIEWER
 )
 
+@register.inclusion_tag('admin/submit_line.html', takes_context=True)
+def submit_row(context):
+    ctx = original_submit_row(context)
+    ctx.update({
+        'show_save_and_add_another': context.get('show_save_and_add_another', ctx['show_save_and_add_another']),
+        'show_save_and_continue': context.get('show_save_and_continue', ctx['show_save_and_continue']),
+        'show_save': context.get('show_save', ctx['show_save'])
+        })
+    return ctx
 
 class ArticleDocumentInline(admin.StackedInline):
     model = ArticleDocument
@@ -36,8 +47,8 @@ class JournalAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         qs = super(JournalAdmin, self).get_queryset(request)
 
-        if not request.user.is_superuser:
-            qs = qs.filter(users=request.user)
+        if request.user.user_type==USER_EDITOR:
+            qs = qs.filter(users=request.user.id)
 
         return qs
 
@@ -61,22 +72,29 @@ class JournalAdmin(admin.ModelAdmin):
 
         return actions
 
-    def get_readonly_fields(self, request, obj=None):
-        actions = super(JournalAdmin, self). get_actions(request)
-
-        if request.user.is_superuser or request.user.user_type==USER_EDITOR:
-            return self.readonly_fields
-        else:
-            return list(set(
-                [field.name for field in self.opts.local_fields] +
-                [field.name for field in self.opts.local_many_to_many]
-            ))
-
     def save_model(self, request, obj, form, change):
         if not request.user.is_superuser or not request.user.user_type==USER_EDITOR:
             obj.user = request.user
 
         obj.save()
+
+        def get_readonly_fields(self, request, obj=None):
+            actions = super(JournalAdmin, self). get_actions(request)
+
+            if request.user.is_superuser or request.user.user_type==USER_EDITOR:
+                return self.readonly_fields
+            else:
+                readonly_fields = ['users', 'name', 'content']
+                return readonly_fields
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        if not request.user.is_superuser and not request.user.user_type==USER_EDITOR:
+            extra_context = extra_context or {}
+            extra_context['show_save_and_add_another'] = False
+            extra_context['show_save_and_continue'] = False
+            extra_context['show_save'] = False
+        return super(JournalAdmin, self).change_view(request, object_id,
+            form_url, extra_context=extra_context)
 
 
 @admin.register(Period)
@@ -93,19 +111,19 @@ class PeriodAdmin(admin.ModelAdmin):
 
         return [field for field in fields if field not in exclude_fields]
 
-    def get_queryset(self, request):
-        qs = super(PeriodAdmin, self).get_queryset(request)
-
-        if not request.user.is_superuser:
-            qs = qs.filter(user=request.user)
-
-        return qs
-
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "journal" and not request.user.is_superuser:
             kwargs["queryset"] = Journal.objects.filter(users=request.user)
 
         return super(PeriodAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def get_queryset(self, request):
+        qs = super(PeriodAdmin, self).get_queryset(request)
+
+        if request.user.user_type==USER_EDITOR:
+            qs = qs.filter(journal__period__user=request.user.id)
+
+        return qs
 
     def has_add_permission(self, request):
         if request.user.is_superuser or request.user.user_type==USER_EDITOR:
@@ -127,22 +145,29 @@ class PeriodAdmin(admin.ModelAdmin):
 
         return actions
 
+    def save_model(self, request, obj, form, change):
+        if not request.user.is_superuser or not request.user.user_type==USER_EDITOR:
+            obj.user = request.user
+
+        obj.save()
+
     def get_readonly_fields(self, request, obj=None):
         actions = super(PeriodAdmin, self). get_actions(request)
 
         if request.user.is_superuser or request.user.user_type==USER_EDITOR:
             return self.readonly_fields
         else:
-            return list(set(
-                [field.name for field in self.opts.local_fields] +
-                [field.name for field in self.opts.local_many_to_many]
-            ))
+            readonly_fields = ['period', 'journal']
+            return readonly_fields
 
-    def save_model(self, request, obj, form, change):
-        if not request.user.is_superuser or not request.user.user_type==USER_EDITOR:
-            obj.user = request.user
-
-        obj.save()
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        if not request.user.is_superuser and not request.user.user_type==USER_EDITOR:
+            extra_context = extra_context or {}
+            extra_context['show_save_and_add_another'] = False
+            extra_context['show_save_and_continue'] = False
+            extra_context['show_save'] = False
+        return super(PeriodAdmin, self).change_view(request, object_id,
+            form_url, extra_context=extra_context)
 
 
 @admin.register(Article)
@@ -182,6 +207,6 @@ class ArticleDocumentAdmin(admin.ModelAdmin):
         qs = super(ArticleDocumentAdmin, self).get_queryset(request)
 
         if not request.user.is_superuser and not request.user.user_type==USER_EDITOR:
-            qs = qs.filter(article__journal__users=request.user.id)
+            qs = qs.filter(article__user=request.user.id)
 
         return qs
